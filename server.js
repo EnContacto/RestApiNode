e0d6578b939ceee0bfc76b8f9a9e457a3dac0a02
Bryan <bryananthony763@gmail.com>
2025-01-13 12:58:39 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
